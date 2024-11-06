fc7879691d5ba45e0c809aa154779e482ec20089
Dimitri <dimitridumonet@gmail.com>
2024-11-06 14:02:00 +0700

    feat: add edit to create blueprint

--- src/app/create/[id]/store.ts
+++ src/app/create/[id]/store.ts
@@ -1,10 +1,18 @@
 import sdk from '@/lib/sdk';
-import { BlueprintProps, DecomposedRegex, DecomposedRegexPart } from '@dimidumo/zk-email-sdk-ts';
+import {
+  Blueprint,
+  BlueprintProps,
+  DecomposedRegex,
+  DecomposedRegexPart,
+} from '@dimidumo/zk-email-sdk-ts';
 import { create } from 'zustand';
 
 type CreateBlueprintState = BlueprintProps & {
+  blueprint: Blueprint | null;
   setField: (field: keyof BlueprintProps, value: any) => void;
   getParsedDecomposedRegexes: () => DecomposedRegex[];
+  setToExistingBlueprint: (id: string) => void;
+  compile: () => Promise<string>;
   submit: () => Promise<string>;
   reset: () => void;
 };
@@ -32,6 +40,8 @@ const initialState: BlueprintProps = {
 
 export const useCreateBlueprintStore = create<CreateBlueprintState>((set, get) => ({
   ...(JSON.parse(JSON.stringify(initialState)) as BlueprintProps),
+
+  blueprint: null,
 
   setField: (field: keyof BlueprintProps, value: any) => set({ [field]: value }),
   getParsedDecomposedRegexes: (): DecomposedRegex[] => {
@@ -65,13 +75,47 @@ export const useCreateBlueprintStore = create<CreateBlueprintState>((set, get) =
     console.log('Cleaned decomposed regex: ', data);
 
     try {
-      const blueprint = sdk.createBlueprint(data);
-      await blueprint.submitDraft();
-      return blueprint.props.id!;
+      // Create a new blueprint
+      if (!state.id || state.id === 'new') {
+        const blueprint = sdk.createBlueprint(data);
+        await blueprint.submitDraft();
+        set({ blueprint });
+        return blueprint.props.id!;
+      }
+
+      // Update an existing blueprint
+      if (state.blueprint && state.blueprint.canUpdate()) {
+        await state.blueprint.update(state);
+        return state.blueprint.props.id!;
+      }
+
+      // Create a new version of an blueprint
+      if (state.blueprint && !state.blueprint.canUpdate()) {
+        await state.blueprint.submitNewVersionDraft(state);
+        return state.blueprint.props.id!;
+      }
+
+      throw new Error('Unknown error saving blueprint');
     } catch (err) {
       console.error('Failed to submit blueprint: ', err);
       throw err;
     }
+  },
+  setToExistingBlueprint: async (id: string) => {
+    try {
+      const blueprint = await sdk.getBlueprint(id);
+      console.log('blueprint: ', blueprint);
+      blueprint.props.decomposedRegexes.forEach((dcr) => {
+        dcr.parts = JSON.stringify(dcr.parts) as unknown as DecomposedRegexPart[];
+      });
+      set({ ...blueprint.props, blueprint });
+    } catch (err) {
+      console.error('Failed to get blueprint for id ', id);
+      throw err;
+    }
+  },
+  compile: async () => {
+    throw new Error('not implemented yet');
   },
   reset: () => set({ ...(JSON.parse(JSON.stringify(initialState)) as BlueprintProps) }),
 }));
