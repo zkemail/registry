402fc71a68f981ae6154caa0398f77f38f57e550
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-12-04 22:10:11 +0530

    :chore: Add verify button in past proofs page

--- src/app/components/ProofStatusTable.tsx
+++ src/app/components/ProofStatusTable.tsx
@@ -11,7 +11,7 @@ const ProofStatusTable = ({ proofs }: { proofs: string[] }) => {
     <div className="overflow-x-auto w-full">
       <div
         className="mb-2 grid gap-4 text-center font-medium text-grey-850 w-[720px]"
-        style={{ gridTemplateColumns: '1.5fr 5fr 1fr 1fr 1fr' }}
+        style={{ gridTemplateColumns: '1.5fr 5fr 1fr 1fr 1.5fr' }}
       >
         <div className="text-left">Proof ID</div>
         <div className="text-left">Outputs</div>
@@ -22,7 +22,7 @@ const ProofStatusTable = ({ proofs }: { proofs: string[] }) => {
 
       <div
         className="grid items-center gap-4 border-t py-2 text-grey-700 w-[720px]"
-        style={{ gridTemplateColumns: '1.5fr 5fr 1fr 1fr 1fr' }}
+        style={{ gridTemplateColumns: '1.5fr 5fr 1fr 1fr 1.5fr' }}
       >
         {blueprint &&
           proofs.map((proofId, index) => (
