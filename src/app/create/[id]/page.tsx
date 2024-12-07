5d8a1e0d1e3e3016194c4c88e0b0a9830d77df41
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-12-07 12:41:31 +0530

    :chore: Update ignore body hash check

--- src/app/create/[id]/page.tsx
+++ src/app/create/[id]/page.tsx
@@ -134,7 +134,7 @@ const CreateBlueprint = ({ params }: { params: Promise<{ id: string }> }) => {
       store.setField('emailQuery', emailQuery);
       store.setField('emailHeaderMaxLength', (Math.ceil(headerLength / 64) + 5) * 64);
       store.setField('emailBodyMaxLength', (Math.ceil(emailBodyMaxLength / 64) + 5) * 64);
-      if (emailBodyMaxLength > 9984) {
+      if (emailBodyMaxLength > 9984 && !store.shaPrecomputeSelector) {
         toast.warning(
           'Email body is too long, max is 10000 bytes. Please add Email body cut off value else skip body hash check'
         );
