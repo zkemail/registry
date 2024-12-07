351a438f1178d7757913668e0b2d583ccdfe01ba
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-12-07 13:40:26 +0530

    :chore: Show external inputs in the generated proof page

--- src/app/[id]/proofs/[proofId]/page.tsx
+++ src/app/[id]/proofs/[proofId]/page.tsx
@@ -130,22 +130,6 @@ const ProofInfo = ({ params }: { params: Promise<{ id: string; proofId: string }
     }
     setIsVerifyingProofLoading(false);
   };
-  
-  const ExternalInputs = () => {
-    if (emailProof && emailProof.externalInputs && emailProof.externalInputs.length) {
-      let externalInputsStr = '';
-      for (const externalInput of JSON.parse(emailProof.externalInputs)) {
-        externalInputsStr += `{"${externalInput.name}": "${externalInput.value}"}`;
-      }
-      return (
-        <div className="flex flex-col justify-between gap-1 md:flex-row">
-          <div className="text-base font-medium text-grey-700">External Inputs</div>
-          <div className="text-base font-medium text-grey-800">{externalInputsStr}</div>
-        </div>
-      );
-    }
-    return <></>;
-  };
 
   return (
     <div className="mx-4 my-16 flex flex-col gap-6 rounded-3xl border border-grey-500 bg-white p-6 shadow-[2px_4px_2px_0px_rgba(0,0,0,0.02),_2px_3px_4.5px_0px_rgba(0,0,0,0.07)]">
@@ -222,7 +206,18 @@ const ProofInfo = ({ params }: { params: Promise<{ id: string; proofId: string }
               : '-'}
           </div>
         </div>
-        <ExternalInputs />
+        {emailProof?.externalInputs && Object.keys(emailProof.externalInputs).length ? (
+          <div className="flex flex-col justify-between gap-1 md:flex-row">
+            <div className="text-base font-medium text-grey-700">External Inputs</div>
+            <div className="text-base font-medium text-grey-800">
+              {emailProof?.externalInputs
+                ? Object.entries(emailProof.externalInputs)
+                    .map(([key, value]) => `{"${key}": "${value}"}`)
+                    .join('\n')
+                : '-'}
+            </div>
+          </div>
+        ) : null}
         <div className="flex flex-col justify-between gap-1 md:flex-row">
           <div className="text-base font-medium text-grey-700">Date created</div>
           <div className="text-base font-medium text-grey-800">
