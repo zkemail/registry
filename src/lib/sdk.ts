b27d12fd82fccbee8bc23a4514f7571ea1e73f4c
Dimitri <dimitridumonet@gmail.com>
2024-12-14 08:38:48 +0700

    chore: bump versions

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,6 +1,6 @@
 import zkeSdk from '@zk-email/sdk';
 import auth from './auth';
 
-const sdk = zkeSdk({ auth, baseUrl: 'http://localhost:8080' });
+const sdk = zkeSdk({ auth });
 
 export default sdk;
