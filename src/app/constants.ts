0055905d323e0eddea35d511c1ed6ad5c4c80216
Shubham Gupta <shubham.gupta2956@gmail.com>
2025-01-14 18:32:27 +0530

    chore: Update UI/UX of regex creation page while creating blueprint

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,3 +4,12 @@ export enum Steps {
   ADD_INPUTS = 'ADD_INPUTS',
   VIEW_PROOF = 'VIEW_PROOF',
 }
+
+export const REGEX_COLORS = [
+  { private: '#F2DA44', public: '#FFF5B4' },
+  { private: '#F58AFF', public: '#FBD1FF' },
+  { private: '#7ACCFF', public: '#BBE5FF' },
+  { private: '#5AFDC7', public: '#B7FFE7' },
+  { private: '#FBAB77', public: '#FFD6BA' },
+  { private: '#C4C4C4', public: '#E2E2E2' },
+];
