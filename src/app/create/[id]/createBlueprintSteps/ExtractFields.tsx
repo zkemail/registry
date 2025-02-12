7174b15633c849f33b5d06cf90422e158be32464
Dimitri <dimitridumonet@gmail.com>
2025-02-12 18:19:59 +0700

    fix: console.log breaking header extraction

--- src/app/create/[id]/createBlueprintSteps/ExtractFields.tsx
+++ src/app/create/[id]/createBlueprintSteps/ExtractFields.tsx
@@ -645,7 +645,6 @@ const ExtractFields = ({
                   isGeneratingFieldsLoading={isGeneratingFieldsLoading[index]}
                 />
                 {regex?.parts?.map((part: any, partIndex: any) => {
-                  console.log(JSON.stringify(regexGeneratedOutputs[index]).includes('Error: '));
                   return (
                     <div key={partIndex} className="flex flex-col gap-3 rounded-lg py-3">
                       <div className="flex items-center justify-between">
@@ -785,10 +784,10 @@ const ExtractFields = ({
                   <>
                     <Label>Output</Label>
                     <div
-                      className={`rounded-lg border text-sm p-2 ${
+                      className={`rounded-lg border p-2 text-sm ${
                         JSON.stringify(regexGeneratedOutputs[index]).includes('Error:')
-                          ? ' border-red-500 bg-red-100'
-                          : ' border-grey-500 bg-neutral-100'
+                          ? 'border-red-500 bg-red-100'
+                          : 'border-grey-500 bg-neutral-100'
                       }`}
                     >
                       {JSON.stringify(regexGeneratedOutputs[index]).includes('Error: ')
