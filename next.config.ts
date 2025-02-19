369a02829cd67579b27e4921739f08419ab8cadd
Dimitri <dimitridumonet@gmail.com>
2025-02-19 18:03:51 +0700

    fix: remove code duplication; update sdk

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: false,
   },
+  productionBrowserSourceMaps: true, // Enable source maps
   webpack: (config, { isServer }) => {
     // This is a browser-only package
     if (!isServer) {
