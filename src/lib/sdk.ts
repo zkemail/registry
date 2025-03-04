a5916da3789c0b7177b179bfff348ec60ad52ae4
Dimitri <dimitridumonet@gmail.com>
2025-03-04 11:27:00 +0700

    --wip-- [skip ci]

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,6 +1,6 @@
 import zkeSdk from '@zk-email/sdk';
 import auth from './auth';
 
-const sdk = zkeSdk({ auth });
+const sdk = zkeSdk({ auth, baseUrl: 'http://localhost:8080' });
 
 export default sdk;
