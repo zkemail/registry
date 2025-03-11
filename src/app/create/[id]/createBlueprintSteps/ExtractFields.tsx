bcb3e1b3f1f05580b6f463925699f42cf6525ab1
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2025-03-11 08:50:09 +0000

    fix: allow user to setup manually max length for each regex pattern

--- src/app/create/[id]/createBlueprintSteps/ExtractFields.tsx
+++ src/app/create/[id]/createBlueprintSteps/ExtractFields.tsx
@@ -149,11 +149,6 @@ const ExtractFields = ({
               updated[index] = regexOutputs;
               return updated;
             });
-
-            // update the max length of the regex at that particular index
-            const decomposedRegexes = [...store.decomposedRegexes];
-            decomposedRegexes[index].maxLength = regexOutputs[0].length ?? 64;
-            setField('decomposedRegexes', decomposedRegexes);
           } catch (error) {
             console.error('Error testing decomposed regex:', error);
             setRegexGeneratedOutputs((prev) => {
