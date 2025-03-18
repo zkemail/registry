49bc2fda57cecd168a383640f1728cdf5afd40a9
Dimitri <dimitridumonet@gmail.com>
2025-03-18 15:02:29 +0700

    feat: make all circuit files downloadable

--- src/app/[id]/download/page.tsx
+++ src/app/[id]/download/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 import sdk from '@/lib/sdk';
-import { Blueprint, DownloadUrls, Status } from '@zk-email/sdk';
+import { Blueprint, ChunkedZkeyUrl, DownloadUrls, Status } from '@zk-email/sdk';
 import { use, useEffect, useState } from 'react';
 import Loader from '@/components/ui/loader';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import Image from "next/image";
+import Image from 'next/image';
 import { useProofStore } from '../store';
 
 const DownloadLinks = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
-  const [downloadLinks, setDownloadLinks] = useState<DownloadUrls | null>();
+  const [downloadLinks, setDownloadLinks] = useState<DownloadUrls | null>(null);
+  const [chunkedDownloadLinks, setChunkedDownloadLinks] = useState<ChunkedZkeyUrl[]>([]);
   const [mainBlueprint, setMainBlueprint] = useState<Blueprint | null>(null);
   const [versions, setVersions] = useState<Blueprint[]>([]);
 
@@ -47,31 +48,37 @@ const DownloadLinks = ({ params }: { params: Promise<{ id: string }> }) => {
     sdk
       .getBlueprintById(id)
       .then((bp) => {
-        return bp.getZKeyDownloadLink();
+        return Promise.all([bp.getZKeyDownloadLink(), bp.getChunkedZkeyDownloadLinks()]);
       })
-      .then(setDownloadLinks)
+      .then(async ([links, chunked]) => {
+        setDownloadLinks(links);
+        setChunkedDownloadLinks(chunked);
+      })
       .catch((err) => {
         console.error('Failed to get blueprint or download linnks: ', err);
       });
   }, []);
 
   return (
-    (<div className="mx-auto flex flex-col gap-10 py-16">
+    <div className="mx-auto flex flex-col gap-2 py-16">
       <div>
         <div className="mb-2 flex items-center justify-between">
           <div className="flex w-full flex-col items-start gap-2">
             <Link href={`/${id}`}>
               <Button
                 variant="ghost"
-                startIcon={<Image
-                  src="/assets/ArrowLeft.svg"
-                  alt="back"
-                  width={16}
-                  height={16}
-                  style={{
-                    maxWidth: "100%",
-                    height: "auto"
-                  }} />}
+                startIcon={
+                  <Image
+                    src="/assets/ArrowLeft.svg"
+                    alt="back"
+                    width={16}
+                    height={16}
+                    style={{
+                      maxWidth: '100%',
+                      height: 'auto',
+                    }}
+                  />
+                }
               >
                 {mainBlueprint?.props.title}
               </Button>
@@ -96,16 +103,46 @@ const DownloadLinks = ({ params }: { params: Promise<{ id: string }> }) => {
                     width={20}
                     height={20}
                     style={{
-                      maxWidth: "100%",
-                      height: "auto"
-                    }} />
+                      maxWidth: '100%',
+                      height: 'auto',
+                    }}
+                  />
                 </Button>
               </div>
             ))
           )}
         </div>
       </div>
-    </div>)
+      {!!chunkedDownloadLinks.length && (
+        <div className="mt-6 flex w-full flex-col justify-between gap-4 md:flex-row">
+          <h2 className="text-xl font-bold">Download Chunked ZKeys used client side</h2>
+        </div>
+      )}
+      <div className="mt-2 flex flex-col gap-4">
+        {!!chunkedDownloadLinks.length &&
+          chunkedDownloadLinks.map(({ suffix, url }) => (
+            <div className="flex items-center justify-between">
+              circuit.zkey{suffix}.gz
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => startDownload(url, `circuit.zkey${suffix}.gz`)}
+              >
+                <Image
+                  src="/assets/Download.svg"
+                  alt="download"
+                  width={20}
+                  height={20}
+                  style={{
+                    maxWidth: '100%',
+                    height: 'auto',
+                  }}
+                />
+              </Button>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };
 
