aa2a8f12c1c5107166e352854fafa08d99cf8e62
Shubham Gupta <shubham.gupta2956@gmail.com>
2025-03-22 13:38:19 +0530

    chore: Clear blueprint creation storage before creation process

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import Image from "next/image";
+import Image from 'next/image';
 import Link from 'next/link';
 import LoginButton from './LoginButton';
 import { Button } from '@/components/ui/button';
 import { useAuthStore } from '@/lib/stores/useAuthStore';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Navbar = () => {
   const token = useAuthStore((state) => state.token);
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
-    (<nav className="flex items-center justify-between border-b bg-white px-6 py-3 dark:border-grey-800 dark:bg-black">
+    <nav className="flex items-center justify-between border-b bg-white px-6 py-3 dark:border-grey-800 dark:bg-black">
       <Link href="/" className="flex items-center gap-2">
         <Image
           src={'/assets/ZKEmailLogoDark.svg'}
@@ -20,22 +21,29 @@ const Navbar = () => {
           height={24}
           width={24}
           style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
         <span className="text-xl font-semibold text-[#1C1C1C] dark:text-white">registry</span>
       </Link>
       <div className="flex items-center gap-4">
         {token && !pathname.includes('/create') ? (
-          <Link className="hidden md:block" href="/create/new">
-            <Button className="rounded-xl">Create Blueprint</Button>
-          </Link>
+          <Button
+            onClick={() => {
+              localStorage.removeItem('create-blueprint');
+              router.push('/create/new');
+            }}
+            className="rounded-xl"
+          >
+            Create Blueprint
+          </Button>
         ) : null}
         <div className="flex items-center gap-4">
           <LoginButton />
         </div>
       </div>
-    </nav>)
+    </nav>
   );
 };
 
