09b96f8e3355cc4baebc732011f9716d37a23861
PrakharSingh0908 <prakharsingh0908@gmail.com>
2025-06-10 21:05:03 +0530

    chore: add truncating values for slug characters

--- src/app/components/BlueprintCard.tsx
+++ src/app/components/BlueprintCard.tsx
@@ -48,8 +48,8 @@ const BlueprintCard = ({ blueprint, setStarred, setUnStarred, starred }: Bluepri
                 }
               </span>
               <span className="hidden md:inline">
-                {blueprint.props.title && blueprint.props.title.length > 60 
-                  ? blueprint.props.title.substring(0, 60) + '...' 
+                {blueprint.props.title && blueprint.props.title.length > 32 
+                  ? blueprint.props.title.substring(0, 32) + '...' 
                   : blueprint.props.title
                 }
               </span>
@@ -109,7 +109,20 @@ const BlueprintCard = ({ blueprint, setStarred, setUnStarred, starred }: Bluepri
             }}
             className="flex cursor-pointer items-center rounded-md border border-neutral-300 bg-neutral-200 px-3 py-1 text-sm font-medium text-grey-800 transition-all hover:border-grey-400 hover:bg-neutral-100"
           >
-            <p className="overflow-hidden text-ellipsis">{blueprint.props.slug}</p>
+            <p className="overflow-hidden text-ellipsis" title={blueprint.props.slug}>
+              <span className="md:hidden">
+                {blueprint.props.slug && blueprint.props.slug.length > 20 
+                  ? blueprint.props.slug.substring(0, 20) + '...' 
+                  : blueprint.props.slug
+                }
+              </span>
+              <span className="hidden md:inline">
+                {blueprint.props.slug && blueprint.props.slug.length > 40 
+                  ? blueprint.props.slug.substring(0, 40) + '...' 
+                  : blueprint.props.slug
+                }
+              </span>
+            </p>
             <div
               className="ml-2 flex items-center justify-center p-0.5"
               aria-label="Copy blueprint reference"
